775faa2b4dcd897a51d258dce715a7f3a03f68ab
obarboza <>
2021-04-23 13:49:14 -0500

    Adding Todo entity validation for JPA entity

--- src/main/java/org/barboza/pastrana/entity/Todo.java
+++ src/main/java/org/barboza/pastrana/entity/Todo.java
@@ -1,12 +1,16 @@
 package org.barboza.pastrana.entity;
 
 import java.time.LocalDate;
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Todo {
@@ -15,9 +19,15 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty(message = "Task must be set")
+    @NotEmpty(message = "Task must be set!")
+    @Size(min=10,message="Task should not be less than 10 characters")
     private String task;
+    
+    @NotNull(message="Duedate must be set")
+    @FutureOrPresent(message = "Duedate must be in the present or future")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     private LocalDate dueDate;
+    
     private boolean isCompleted;
     private LocalDate dateCompleted;
     private LocalDate dateCreated;
